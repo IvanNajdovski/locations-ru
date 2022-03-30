667a887e6e479e402cdc354377831ec04c5c3487
Ivan Najdovski <ivan.najdovski@keitaro.com>
2022-03-30 13:55:15 +0200

    Add changes

--- src/components/RegionSelect.js
+++ src/components/RegionSelect.js
@@ -3,18 +3,21 @@ import { useContext } from "react";
 import Select from "react-select";
 import AppContext from "../state";
 
+const customStyles = {
+  menuList: (provided, state) => ({
+    ...provided,
+    "&>*:last-child": {
+      paddingTop: ".5rem",
+      borderTop: "2px solid #000",
+    },
+  }),
+};
 const RegionSelect = (props) => {
   const { region, setRegion } = useContext(AppContext);
 
   return (
     <div>
-      <Select
-        options={props.options}
-        value={region}
-        onChange={setRegion}
-        placeholder="Регион деятельности"
-        isSearchable
-      />
+      <Select styles={customStyles} options={props.options} value={region} onChange={setRegion} placeholder="Регион деятельности" isSearchable />
     </div>
   );
 };
